f689abe427e8bbb7021bcd237d87fa1be20a58e4
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-08 16:38:37 +0700

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+// จงเขียน Function นำตัวเลขทั้งสองตัวมายกกำลังกัน แล้ว return ผลลัพธ์ออกมา
+
+function multi(x, y) {
+    return x ** y;
+}
+
+console.log(multi(2));
+console.log(multi(2, 2));
+console.log(multi(5, 3));
+console.log(multi(12, 2));
